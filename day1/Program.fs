beac7a1ce5b0ec6495bceb4e5fadc81a5d2587b5
Andrew Alkema <aalkema@gmail.com>
2024-12-01 22:33:42 +0000

    message

--- day1/Program.fs
+++ day1/Program.fs
@@ -1,2 +1,15 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System 
+
+let parseLine (input:string) =
+    let split = input.Split [|' '|]
+    let id, zipCode = split.[0], Int32.Parse split.[1]
+    let firstList = split.[0]
+    let secondList = split.[1]
+    (firstList, secondList)
+
+let printLine (id, zipCode, street) = sprintf "%s|%i|%s" id zipCode street
+
+let lines = 
+    IO.File.ReadLines @"./input.txt"
+    |> Seq.map parseLine
+
